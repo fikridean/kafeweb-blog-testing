6e873dfdcfe4798204c0fff08d4664eb78f9dfa0
FikriDean <fkrdean@gmail.com>
2022-08-12 20:14:22 +0700

    debug

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,12 +7,12 @@
   <div class="row">
     <div class="col-xl-12">
 
-      <div class="card mt-4 bg-body border border-0">
-        <div class="card-header fw-semibold bg-body border border-0">
+      <div class="card mt-4 bg-dark border border-0 text-light">
+        <div class="card-header fw-semibold bg-dark border border-0 text-light">
           @<span></span>{{ $user->username }}
         </div>
 
-        <div class="card-body bg-body border border-0">
+        <div class="card-body bg-dark border border-0 text-light">
           <h3 class="card-title">{{ $user->name }}</h3>
           <p class="card-text mt-3"><i class="bi bi-envelope-check-fill"></i> {{ $user->email }}</p>
           <p class="card-text">joined since {{ $user->created_at->diffForHumans() }}</p>
