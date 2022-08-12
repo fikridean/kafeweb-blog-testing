42311d60c96003f5402bda19c526c7cf422a3d06
FikriDean <fkrdean@gmail.com>
2022-08-12 21:22:38 +0700

    test

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -61,7 +61,7 @@
               <img class="rounded-circle border border-1 border-light p-1" src="{{ asset('storage/' . $comment->user->image)}}" alt="Profile Photo" style="width: 20px; height:20px">
             </div>
             <div class="mx-2 d-flex flex-column align-items-start">
-              <small class="text-muted">{{ $comment->user->name }} • {{ $comment->updated_at->diffForHumans() }}</small>
+              <small class="text-muted text-start">{{ $comment->user->name }} • {{ $comment->updated_at->diffForHumans() }}</small>
               <p class="p-0 m-0 text-start">{{ $comment->body }}</p>
               @if ($comment->created_at != $comment->updated_at)
                 <small class="text-muted">Edited</small>  
